b62c595cea282ded96432f9277ccc3842a7a7587
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-25 23:45:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < Sinatra::Base
     erb :new 
   end 
   
+  
   post '/articles' do 
     @article = Article.create(params)
     redirect to "/articles/#{@article.id}"
