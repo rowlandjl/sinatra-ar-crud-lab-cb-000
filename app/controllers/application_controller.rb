64c1c04649e2924db7f0fc33ac0e3fcb454a21d3
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-25 23:47:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,6 +49,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{@article.id}"
   end 
   
+  # destroy 
   delete '/articles/:id' do 
     Article.destroy(params[:id])
     redirect to "/articles"
